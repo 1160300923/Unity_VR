d81a0da71fbdcdfcd17a5b9acf803dbc6f117c7a
1160300923 <909989562@qq.com>
2019-11-28 18:11:10 +0800

    Version2

---
+++ MailPilot/Assets/MailSetting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailSetting : MonoBehaviour
+{
+    public TweenPosition mailboxTween;
+    public TweenPosition mailOneTween;
+    
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnMailOneButtonClick()
+    {
+        mailboxTween.PlayForward();
+        mailOneTween.PlayForward();
+    }
+    public void OnMailOneBackButtonClick()
+    {
+        mailboxTween.PlayReverse();
+        mailOneTween.PlayReverse();
+    }
+}
